cac538e72eba48db1c4930314cdd157a115250e7
mrodriguez <mrodriguez@canoga.com>
2023-02-09 14:51:34 -0800

    added collapable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,6 +42,14 @@ import { LoadingSpinnerComponent } from './components/loading-spinner/loading-sp
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
 
     // Binding the store.The store contains only a single reducer.
     StoreModule.forRoot(appReducer),
@@ -38,6 +58,7 @@ import { LoadingSpinnerComponent } from './components/loading-spinner/loading-sp
     // Instrumentation must be imported after importing StoreModule (config is optional)
     StoreDevtoolsModule.instrument({ maxAge: 25, // Retains last 25 states.
       logOnly: environment.production }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+import {FormGroup,FormControl,Validators,FormArray} from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  panelOpenState = false;
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+
+    /**
+     * The following example shows that once a value of map has bain captured, you
+     * can modify the value and there is no ned to set the map with the new value
+     * since the the change is made to the object reference, run code to see and undesrtand more.
+     *
+     * https://www.typescriptlang.org/play?ssl=28&ssc=8&pln=1&pc=1#code/FAYw9gdgzgLgBBArgWwDYEtYC4nIEYCmATnANoC6cAvGQIwA0ATPQMyvkDcwwqB8yAQwAOQ4ljgBZYQB5YRdBADm9BCkJEKAPmoICAd0nCAFAEouwXBlgA6AGZgiAUQEgAFkYLVtAb25x-cLzwAG4gAJIAJjpSMK52qGAORjFxRAIQEWDIpnAAVHC0AAwm1jBgAMow8kqmwAFwAPQN4NBgvNYJikahkSZ+AUFwoQAifALoqFDiuOoUAD6IGQS2CgRRNIIixNaKfN3hEWb9-i1QbQQdYF0A5IT2RJ7XcADUQyCjMOOTR3UB6LZwIwAQhGYwmUBMcF89XqTTgADkwHABEQ0gBPOBovgqM5wEAPAQwTyQTzpKKwByedDwe5wWKYOAANQAwr8YW8Pl8oDoKFx2f5NqIiNYoHsehEVKDPuCjuyAL5sgJwgCCESi9O5ETBqDpSI1TNZ7KlXOsQkQUHcBFlMNO50uNwEtiJJGuz2NMvMcp+gu292cbiMtkWIBg6EggOCAlQiAI0zUxAoKgA1gQ0eI5ApFJDoTbIGd2p0jCmMa8AERwcSlt1RmPW-xe8xwwY+ojiKRCWRVTMqGYJ8jaGgQfSGISmRsNQJ8BC0HDxjTkBZLFZD9ZwFs7PbXexga59YBw-7AiC0Ey+OH1Y88zj7if1deimBGUvb0s9k83uAKj-H03m9yME8uDhH8zQtIwWAAVh+JspwgRhZ3wPtFy1Zc1h0dddkfZ9ElLaCJzgoE7Acf1LS8PE8ztQsrT3bggA
+     */
+    const numlist: number[] = [1, 2, 3, 3];
+
+    let mapper: Map<string, number[]> = new Map();
+
+    numlist.forEach(e => {
+
+      let vcId = Math.floor(Math.random() * 10).toString()
+      //console.log(vcId)
+
+      let vcDetails: number[] | undefined = mapper.get(vcId);
+
+      console.log('before ' + vcDetails);
+
+      if (!vcDetails) {
+        // No array yet, so create one and store it for this VC
+        vcDetails = [];
+        mapper.set(vcId, vcDetails);
+      }
+      // Add this detail to this VC
+      vcDetails.push(e);
+      console.log('after ' + vcDetails);
+
+    });
+
+    mapper.forEach(function (value: number[], key: string) {
+      console.log(key + " : " + value);
+    });
   }
+
 
 }
